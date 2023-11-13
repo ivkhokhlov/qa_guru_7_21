519334fb43c9eafaf1a376a8a44deae0ff6da374
Ivan Khokhlov <xxx.mailbox73@gmail.com>
2023-11-13 12:31:10 +0400

    HW-21

---
+++ tests/mobile/android/test_wiki_android.py
@@ -0,0 +1,32 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+from tests.mobile.conftest import android
+import allure
+
+@android
+def test_search():
+
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+
+    with allure.step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+@android
+def test_search_and_open_article():
+
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+
+    with allure.step('Open the first article on the screen'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
+
+    with allure.step('Verify content in the article'):
+        results = browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/view_wiki_error_text'))
+        results.should(have.text('An error occurred'))
---
+++ tests/mobile/conftest.py
@@ -0,0 +1,82 @@
+import os
+
+import allure
+import allure_commons
+import pytest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.options.ios import XCUITestOptions
+from selene import browser, support
+from dotenv import load_dotenv
+
+from config import config
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_end():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management(request):
+    user_name = os.getenv('BSTACK_USER')
+    access_key = os.getenv('BSTACK_ACCESS_KEY')
+
+    if request.param == 'Android':
+        options = UiAutomator2Options().load_capabilities({
+            "platformName": "android",
+            "platformVersion": "9.0",
+            "deviceName": "Google Pixel 3",
+
+            "app": config.app_id,
+
+            'bstack:options': {
+                "projectName": "First Python project",
+                "buildName": "browserstack-build-1",
+                "sessionName": "BStack first_test",
+
+                "userName": user_name,
+                "accessKey": access_key
+            }
+        })
+    else:
+        options = XCUITestOptions().load_capabilities({
+            "app": config.app_id,
+
+            "deviceName": "iPhone 11 Pro",
+            "platformName": "ios",
+            "platformVersion": "13",
+
+            "bstack:options": {
+                "userName": user_name,
+                "accessKey": access_key,
+                "projectName": "First Python project",
+                "buildName": "browserstack-build-1",
+                "sessionName": "BStack first_test"
+            }
+        })
+    with allure.step('Init app session'):
+        browser.config.driver = webdriver.Remote(
+            config.browser_url,
+            options=options
+        )
+
+    browser.config.timeout = config.timeout
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+
+    browser.config.driver_remote_url = config.browser_url
+    browser.config.driver_options = options
+
+    browser.config.timeout = config.timeout
+
+    yield
+
+    browser.quit()
+
+
+# Device name parameters
+ios = pytest.mark.parametrize('mobile_management', ['IOS'], indirect=True)
+android = pytest.mark.parametrize('mobile_management', ['Android'], indirect=True)
